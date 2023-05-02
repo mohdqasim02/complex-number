9903db5bfd4e953f82097c3ec98612316429fbcc
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-05-02 19:23:34 +0530

    Add feature to add, multiply

--- src/complex-numbers.js
+++ src/complex-numbers.js
@@ -1,3 +1,6 @@
+const complexNumber = function() {
+  const iotaSq = -1;
+};
 const getRealPart = function(complexNum) {
   return complexNum.real;
 };
@@ -6,5 +9,28 @@ const getImaginaryPart = function(complexNum) {
   return complexNum.imaginary;
 };
 
+const add = function(num1, num2) {
+  return {
+    real: num1.real + num2.real,
+    imaginary: num1.imaginary + num2.imaginary
+  };
+};
+
+const multiply = function(num1, num2) {
+  let real = num1.real * num2.real;
+  real += num1.imaginary * num2.imaginary * -1;
+  let imaginary = num1.real * num2.imaginary;
+  imaginary += num1.imaginary * num2.real;
+
+  return {real, imaginary};
+};
+
+const display = function(complexNum) {
+  return `${complexNum.real} + ${complexNum.imaginary}i`;
+};
+
+exports.add = add;
+exports.multiply = multiply;
+exports.display = display;
 exports.getRealPart = getRealPart;
 exports.getImaginaryPart = getImaginaryPart;
--- tests/complex-numbers-test.js
+++ tests/complex-numbers-test.js
@@ -1,37 +1,43 @@
 const {it, describe} = require('node:test');
-const {strictEqual} = require('assert');
-const {getRealPart, getImaginaryPart} = require('../src/complex-numbers.js');
+const {strictEqual, deepStrictEqual} = require('assert');
+const {getRealPart, getImaginaryPart, add, multiply, display} = require('../src/complex-numbers.js');
 
 describe('complex-numbers', function() {
   describe('getRealPart', function() {
     it('should give real part', function() {
-      strictEqual(getRealPart({
-        real: 2,
-        imaginary: 3,
-      }), 2);
+      strictEqual(getRealPart({real: 2, imaginary: 3}), 2);
     });
 
     it('should give real part', function() {
-      strictEqual(getRealPart({
-        real: 0,
-        imaginary: 3,
-      }), 0);
+      strictEqual(getRealPart({real: 0, imaginary: 3}), 0);
     });
   });
 
   describe('getImaginaryPart', function() {
     it('should give Imaginary part', function() {
-      strictEqual(getImaginaryPart({
-        real: 2,
-        imaginary: 3,
-      }), 3);
+      strictEqual(getImaginaryPart({real: 2, imaginary: 3}), 3);
     });
 
     it('should give Imaginary part', function() {
-      strictEqual(getImaginaryPart({
-        real: 0,
-        imaginary: 3,
-      }), 3);
+      strictEqual(getImaginaryPart({real: 0, imaginary: 3}), 3);
+    });
+  });
+
+  describe('add', function() {
+    it('should give sum', function() {
+      deepStrictEqual(add({real: 2, imaginary: 3}, {real: 4, imaginary: 5}), {real: 6, imaginary: 8});
+    });
+  });
+
+  describe('multiply', function() {
+    it('should give product', function() {
+      deepStrictEqual(multiply({real: 2, imaginary: 3}, {real: 1, imaginary: 2}), {real: -4, imaginary: 7});
+    });
+  });
+
+  describe('display', function() {
+    it('should display', function() {
+      deepStrictEqual(display({real: 4, imaginary: 5}), '4 + 5i');
     });
   });
 });
